80f043508a5041f80c7806873d2f40e2652ea32e
REDDEAD <hsesgamer@outlook.com>
2025-08-25 17:53:36 -0300

    fix: corrige requisições para spring boot

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -23,9 +24,27 @@ export class LoginComponent {
     this.sectionAtual = section;
   }
 
+  constructor(private http: HttpClient) {}
   onLogin() {
     if (this.loginData.email && this.loginData.senha) {
       console.log('Login enviado:', this.loginData);
+
+      const loginJson = JSON.stringify(this.loginData);
+
+      // duvida se é post ou get
+      this.http.post('http://localhost:8080/login', loginJson).subscribe({
+        next: (res) => {
+          console.log('Login enviado com sucesso!', res);
+        },
+        error: (err) => {
+          console.error('Erro ao enviar login:', err);
+        },
+        complete: () => {
+          console.log('Requisição finalizada.');
+        },
+      });
+    } else {
+      console.warn('Email e senha são obrigatórios!');
     }
   }
 
@@ -34,12 +53,43 @@ export class LoginComponent {
       this.cadastroData;
     if (nome && cpf && data_nascimento && telefone && email && senha) {
       console.log('Cadastro enviado:', this.cadastroData);
+
+      const cadastroJson = JSON.stringify(this.cadastroData);
+      this.http.post('http://localhost:8080/cadastro', cadastroJson).subscribe({
+        next: (res) => {
+          console.log('Cadastro enviado com sucesso!', res);
+        },
+        error: (err) => {
+          console.error('Erro ao enviar cadastro:', err);
+        },
+        complete: () => {
+          console.log('Requisição finalizada.');
+        },
+      });
+    } else {
+      console.warn('Todos os campos são obrigatórios!');
     }
   }
 
   onRecuperar() {
     if (this.recuperarData.email_recuperar) {
       console.log('Recuperar senha:', this.recuperarData);
+      const recuperarJson = JSON.stringify(this.recuperarData);
+      this.http
+        .post('http://localhost:8080/recuperar', recuperarJson)
+        .subscribe({
+          next: (res) => {
+            console.log('Pedido de recuperação enviado com sucesso!', res);
+          },
+          error: (err) => {
+            console.error('Erro ao enviar pedido de recuperação:', err);
+          },
+          complete: () => {
+            console.log('Requisição finalizada.');
+          },
+        });
+    } else {
+      console.warn('Email é obrigatório!');
     }
   }
 }

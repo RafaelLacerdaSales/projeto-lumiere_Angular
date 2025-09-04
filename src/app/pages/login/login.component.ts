8aa8c8ad846e12bd7d9e2d4e1dde8bfa5818eb4a
REDDEAD <hsesgamer@outlook.com>
2025-09-04 12:42:20 -0300

    Style: Botão de login consertado

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { LumiereService } from 'src/app/Service/lumiere.service';
 import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,68 +12,76 @@ import { FormBuilder } from '@angular/forms';
 export class LoginComponent {
   sectionAtual: 'login' | 'cadastro' | 'recuperar' = 'login';
 
-  constructor (private lumiereService: LumiereService, private fb: FormBuilder) {}
-    chaveInicial: boolean = false;
-    nome: String = '';
-    cpf: number = 0;
-    data_nascimento: String = '';
-    telefone: String = '';
-    email: String = '';
-    senha: String = '';
+  constructor(
+    private lumiereService: LumiereService,
+    private fb: FormBuilder,
+    private router: Router
+  ) {}
+  chaveInicial: boolean = false;
+  nome: String = '';
+  cpf: number = 0;
+  data_nascimento: String = '';
+  telefone: String = '';
+  email: String = '';
+  senha: String = '';
 
-    submitUser(){
+  submitUser() {
     const user = {
       nome: this.nome,
       cpf: this.cpf,
       data_nascimento: this.data_nascimento,
       telefone: this.telefone,
       email: this.email,
-      senha: this.senha
-    }
+      senha: this.senha,
+    };
 
     this.lumiereService.cadastrarUser(user).subscribe({
       next: (response) => {
-        if(response.sucesso){
-          alert(response.sucesso)
+        if (response.sucesso) {
+          alert(response.sucesso);
         }
       },
       error: (err) => {
-        if(err.error){
-            alert(err.error.error )
+        if (err.error) {
+          alert(err.error.error);
         }
-      }
-    })
+      },
+    });
   }
-  
 
   recuperarData = { email_recuperar: '' };
 
   mudarSection(section: 'login' | 'cadastro' | 'recuperar') {
     this.sectionAtual = section;
   }
-  
+
   onLogin() {
-   const user = {
+    const user = {
       loginValidar: this.chaveInicial,
       email: this.email,
-      senha: this.senha
-    }
+      senha: this.senha,
+    };
 
     this.lumiereService.validarUser(user).subscribe({
       next: (response) => {
-         if(response.loginValidar){
-        localStorage.setItem("token", "validado")
-      }},
-      error: (error) => {
-        if(error.error){
-          alert("Login invalido"); 
+        if (response.loginValidar) {
+          localStorage.setItem('token', 'validado');
+
+          window.dispatchEvent(new Event('storage'));
+          this.router.navigate(['/workshop']);
         }
-      }
-    })
+      },
+      error: (error) => {
+        if (error.error) {
+          alert('Login invalido');
+        }
+      },
+    });
   }
 
   onRecuperar() {
     if (this.recuperarData.email_recuperar) {
-      console.log("estou aqui")
+      console.log('estou aqui');
+    }
   }
-  }}
+}

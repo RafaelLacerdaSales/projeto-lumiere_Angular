dac53dd82b67cd07b73623a0330f07cf4de327f7
REDDEAD <hsesgamer@outlook.com>
2025-09-08 16:22:19 -0300

    ADM tabela

--- src/app/pages/adm/adm.component.ts
+++ src/app/pages/adm/adm.component.ts
@@ -8,7 +8,11 @@ import { WorkshopServiceService } from 'src/app/Service/workshop-service.service
   styleUrls: ['./adm.component.css'],
 })
 export class AdmComponent {
-  constructor(private workshopService: WorkshopServiceService,  private fb: FormBuilder) { }
+  constructor(
+    private workshopService: WorkshopServiceService,
+    private fb: FormBuilder
+  ) {}
+  cursos: any[] = [];
   id: number = 0;
   tituloDoCurso: String = ``;
   descricao: String = ``;
@@ -20,48 +24,57 @@ export class AdmComponent {
     const videosWorkshop = document.getElementById('videosWorkshop');
     const formCadastro = document.getElementById('formCadastro');
     const containerAddVideios = document.getElementById('containerAddVideios');
+    const tabela = document.getElementById('tabela');
+    const tabela_videios = document.getElementById('tabela_videios');
 
     videosWorkshop?.addEventListener('click', (e) => {
       e.preventDefault();
-      if (formCadastro && containerAddVideios) {
+      if (formCadastro && tabela_videios && containerAddVideios) {
+        tabela_videios.style.display = 'none';
         formCadastro.style.display = 'none';
         containerAddVideios.style.display = 'block';
       }
     });
     cadastro?.addEventListener('click', (e) => {
       e.preventDefault();
-      if (containerAddVideios && formCadastro) {
+      if (containerAddVideios && formCadastro && tabela_videios) {
         containerAddVideios.style.display = 'none';
         formCadastro.style.display = 'block';
+        tabela_videios.style.display = 'none';
+      }
+    });
+    tabela?.addEventListener('click', (e) => {
+      e.preventDefault();
+      if (formCadastro && tabela_videios && containerAddVideios) {
+        containerAddVideios.style.display = 'none';
+        formCadastro.style.display = 'none';
+        tabela_videios.style.display = 'block';
       }
     });
   }
 
   cadastrarCursos() {
-    console.log("entrei 1 etapa")
+    console.log('entrei 1 etapa');
     const dadosCursos = {
       tituloDoCurso: this.tituloDoCurso,
       descricao: this.descricao,
       preco: this.preco,
-      caminhoDaCapa: this.caminhoDaCapa
-    }
+      caminhoDaCapa: this.caminhoDaCapa,
+    };
 
     this.workshopService.cadastrarCurso(dadosCursos).subscribe({
       next: (response) => {
-         console.log("entrei 2 etapa")
+        console.log('entrei 2 etapa');
         if (response.sucesso) {
-           console.log("entrei 3 etapa")
-          alert(response.sucesso)
+          console.log('entrei 3 etapa');
+          alert(response.sucesso);
         }
       },
       error: (err) => {
         if (err.error) {
-          alert(err.error.error)
+          alert(err.error.error);
         }
-      }
-    })
+      },
+    });
   }
-
-
-
 }

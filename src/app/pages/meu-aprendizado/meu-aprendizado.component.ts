8af2fa0d1eaa2675b38fc7d4eeb1bec4bbeb0714
REDDEAD <hsesgamer@outlook.com>
2025-09-23 17:54:57 -0300

    2.0

--- src/app/pages/meu-aprendizado/meu-aprendizado.component.ts
+++ src/app/pages/meu-aprendizado/meu-aprendizado.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
 
+interface Curso {
+  nome: string;
+  imagem: string;
+}
+
 @Component({
   selector: 'app-meu-aprendizado',
   templateUrl: './meu-aprendizado.component.html',
   styleUrls: ['./meu-aprendizado.component.css'],
 })
-export class MeuAprendizadoComponent {}
+export class MeuAprendizadoComponent {
+  // Dados mocados para os cursos. Idealmente, viriam de um serviço.
+  cursos: Curso[] = [
+    { nome: 'Maquiagem Profissional', imagem: '/assets/img_Maquiagem.png' },
+    { nome: 'Maquiagem para Festas', imagem: '/assets/img_Maquiagem.png' },
+    { nome: 'Maquiagem Artística', imagem: '/assets/img_Maquiagem.png' },
+    { nome: 'Automaquiagem', imagem: '/assets/img_Maquiagem.png' },
+    { nome: 'Técnicas Avançadas', imagem: '/assets/img_Maquiagem.png' },
+    { nome: 'Maquiagem para Noivas', imagem: '/assets/img_Maquiagem.png' },
+  ];
+}
